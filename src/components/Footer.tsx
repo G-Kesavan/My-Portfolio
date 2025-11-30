2e93cedc310bf979e8ee391141c772d7ae43b873
Kesava perumal G <Mr.kesavaperumal@gmail.com>
2025-12-01 01:45:29 +0530

    Delete src directory

--- src/components/Footer.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Footer = () => {
-  return (
-    <div className='w-full h-[200px]'>Footer</div>
-  )
-}
-
-export default Footer
--- src/components/Menu.tsx
+++
@@ -1,7 +0,0 @@
-export default function Navbar() {
-  return (
-    <div>
-      <button className="menu-button">menu</button>
-    </div>
-  );
-}
