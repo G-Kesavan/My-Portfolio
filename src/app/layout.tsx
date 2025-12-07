80ac5e43172740dc5c3b4ba341ab734d9864aa34
G-Kesavan <mr.kesavaperumal@gmail.com>
2025-12-07 22:21:11 +0530

    test

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import LiquidEther from "@/components/LiquidEther";
 import "@/styles/globals.css";
 import "@/styles/variables.css";
 
@@ -9,6 +10,25 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className="relative min-h-screen">
+        <div style={{ width: "100%", height: "100%", position: "fixed", zIndex: -1}}>
+          <LiquidEther
+            colors={["#5227FF", "#FF9FFC", "#B19EEF"]}
+            mouseForce={20}
+            cursorSize={100}
+            isViscous={false}
+            viscous={30}
+            iterationsViscous={32}
+            iterationsPoisson={32}
+            resolution={0.5}
+            isBounce={false}
+            autoDemo={true}
+            autoSpeed={0.5}
+            autoIntensity={2.2}
+            takeoverDuration={0.25}
+            autoResumeDelay={3000}
+            autoRampDuration={0.6}
+          />
+        </div>
         {children}
       </body>
     </html>
